af2abfce173334ab8a8cb9735526fcbc245ec2b7
Stella Nthenya <stellaalogi@gmail.com>
2023-05-22 23:16:18 +0300

    done

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
+// react-dom package
+// It takes in two arguments:
+
+// A React component to render (typically, we'll render our top-level App component here).
+// A DOM element where we want that component to be rendered (by convention, a div with the ID of root).
 ReactDOM.render(<App />, document.getElementById("root"));
